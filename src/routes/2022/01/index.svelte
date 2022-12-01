fa47cfb0f541424a6ce18d1889f9fed6a88859ab
formotiis <nosvulca@gmail.com>
2022-12-01 08:15:36 +0100

    Last year + this year

---
+++ src/routes/2022/01/index.svelte
@@ -0,0 +1,36 @@
+<script lang='ts'>
+
+	import { Day01Real, Day01Test } from './dataset';
+
+	const real = Day01Real;
+	const test = Day01Test;
+
+	let isRealDataset = false;
+
+	function sortedTotals(input: string){
+		const packs: string[] = input.split("\n\n");
+		const totals: number[] = packs.map(pack => pack.split("\n").map(string => +string).reduce((a,c) => a + c))
+		return totals.sort((a, b) => b - a);
+	}
+
+	function mostCalories(input: string): number {
+		return sortedTotals(input)[0];
+	}
+
+	function mostCalories3(input: string): number {
+		return sortedTotals(input)[0] +sortedTotals(input)[1] + sortedTotals(input)[2];
+	}
+
+
+
+</script>
+
+<h1>Day 01</h1>
+<label>
+	<input type=checkbox bind:value={isRealDataset}>
+	Toggle Real Data
+</label>
+<p class="lead"> Results on First: {isRealDataset? 'Real': 'Test'} dataset</p>
+<p>{mostCalories(isRealDataset ? real : test)}</p>
+<p class="lead"> Results on Second: {isRealDataset? 'Real': 'Test'} dataset</p>
+<p>{mostCalories3(isRealDataset ? real : test)}</p>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,66 @@
+<script lang='ts'>
+
+	const years = [];
+
+	let year;
+	let day;
+
+	let answer = '';
+
+</script>
+
 <h1>Advent of Code</h1>
-<p>Day <a href='/day/01'>01</a></p>
-<p>Day <a href='/day/02'>02</a></p>
-<p>Day <a href='/day/03'>03</a></p>
-<p>Day <a href='/day/04'>04</a></p>
-<p>Day <a href='/day/05'>05</a></p>
-<p>Day <a href='/day/06'>06</a></p>
-<p>Day <a href='/day/07'>07</a></p>
-<p>Day <a href='/day/08'>08</a></p>
-<p>Day <a href='/day/09'>09</a></p>
-<p>Day <a href='/day/10'>10</a></p>
-<p>Day <a href='/day/11'>11</a></p>
-<p>Day <a href='/day/12'>12</a></p>
-<p>Day <a href='/day/13'>13</a></p>
-<p>Day <a href='/day/14'>14</a></p>
-<p>Day <a href='/day/15'>15</a></p>
-<p>Day <a href='/day/16'>16</a></p>
-<p>Day <a href='/day/17'>17</a></p>
+
+
+
+<p>2021 day <a href='/2021/01'>01</a></p>
+<p>2021 day <a href='/2021/02'>02</a></p>
+<p>2021 day <a href='/2021/03'>03</a></p>
+<p>2021 day <a href='/2021/04'>04</a></p>
+<p>2021 day <a href='/2021/05'>05</a></p>
+<p>2021 day <a href='/2021/06'>06</a></p>
+<p>2021 day <a href='/2021/07'>07</a></p>
+<p>2021 day <a href='/2021/08'>08</a></p>
+<p>2021 day <a href='/2021/09'>09</a></p>
+<p>2021 day <a href='/2021/10'>10</a></p>
+<p>2021 day <a href='/2021/11'>11</a></p>
+<p>2021 day <a href='/2021/12'>12</a></p>
+<p>2021 day <a href='/2021/13'>13</a></p>
+<p>2021 day <a href='/2021/14'>14</a></p>
+<p>2021 day <a href='/2021/15'>15</a></p>
+<p>2021 day <a href='/2021/16'>16</a></p>
+<p>2021 day <a href='/2021/17'>17</a></p>
+<p>2021 day <a href='/2021/18'>18</a></p>
+<p>2021 day <a href='/2021/19'>19</a></p>
+<p>2021 day <a href='/2021/20'>20</a></p>
+<p>2021 day <a href='/2021/21'>21</a></p>
+<p>2021 day <a href='/2021/22'>22</a></p>
+<p>2021 day <a href='/2021/23'>23</a></p>
+<p>2021 day <a href='/2021/24'>24</a></p>
+<p>2021 day <a href='/2021/25'>25</a></p>
+
+<p>2022 day <a href='/2022/01'>01</a></p>
+<p>2022 day <a href='/2022/02'>02</a></p>
+<p>2022 day <a href='/2022/03'>03</a></p>
+<p>2022 day <a href='/2022/04'>04</a></p>
+<p>2022 day <a href='/2022/05'>05</a></p>
+<p>2022 day <a href='/2022/06'>06</a></p>
+<p>2022 day <a href='/2022/07'>07</a></p>
+<p>2022 day <a href='/2022/08'>08</a></p>
+<p>2022 day <a href='/2022/09'>09</a></p>
+<p>2022 day <a href='/2022/10'>10</a></p>
+<p>2022 day <a href='/2022/11'>11</a></p>
+<p>2022 day <a href='/2022/12'>12</a></p>
+<p>2022 day <a href='/2022/13'>13</a></p>
+<p>2022 day <a href='/2022/14'>14</a></p>
+<p>2022 day <a href='/2022/15'>15</a></p>
+<p>2022 day <a href='/2022/16'>16</a></p>
+<p>2022 day <a href='/2022/17'>17</a></p>
+<p>2022 day <a href='/2022/18'>18</a></p>
+<p>2022 day <a href='/2022/19'>19</a></p>
+<p>2022 day <a href='/2022/20'>20</a></p>
+<p>2022 day <a href='/2022/21'>21</a></p>
+<p>2022 day <a href='/2022/22'>22</a></p>
+<p>2022 day <a href='/2022/23'>23</a></p>
+<p>2022 day <a href='/2022/24'>24</a></p>
+<p>2022 day <a href='/2022/25'>25</a></p>
