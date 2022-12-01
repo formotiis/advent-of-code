dd588d0dbc0b2f46f04476c311facb3c73a60c10
formotiis <nosvulca@gmail.com>
2022-12-01 08:50:46 +0100

    Lobby update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,66 +1,34 @@
 <script lang='ts'>
 
-	const years = [];
+	const years = [2021, 2022];
+	const days: string[] = Array.from({length: 25}, (item, index) => ('00'+ `${index+1}`)
+		.substring(`${index}`.length,`${index}`.length+2));
 
-	let year;
-	let day;
-
-	let answer = '';
+	let year = 2022;
+	let day = '01';
 
 </script>
 
 <h1>Advent of Code</h1>
 
+Year
+<select bind:value={year}>
+	{#each years as y}
+		<option value={y}>
+			{y}
+		</option>
+	{/each}
+</select>
+
+Day
+<select bind:value={day}>
+	{#each days as d}
+		<option value={d}>
+			{d}
+		</option>
+	{/each}
+</select>
 
 
-<p>2021 day <a href='/2021/01'>01</a></p>
-<p>2021 day <a href='/2021/02'>02</a></p>
-<p>2021 day <a href='/2021/03'>03</a></p>
-<p>2021 day <a href='/2021/04'>04</a></p>
-<p>2021 day <a href='/2021/05'>05</a></p>
-<p>2021 day <a href='/2021/06'>06</a></p>
-<p>2021 day <a href='/2021/07'>07</a></p>
-<p>2021 day <a href='/2021/08'>08</a></p>
-<p>2021 day <a href='/2021/09'>09</a></p>
-<p>2021 day <a href='/2021/10'>10</a></p>
-<p>2021 day <a href='/2021/11'>11</a></p>
-<p>2021 day <a href='/2021/12'>12</a></p>
-<p>2021 day <a href='/2021/13'>13</a></p>
-<p>2021 day <a href='/2021/14'>14</a></p>
-<p>2021 day <a href='/2021/15'>15</a></p>
-<p>2021 day <a href='/2021/16'>16</a></p>
-<p>2021 day <a href='/2021/17'>17</a></p>
-<p>2021 day <a href='/2021/18'>18</a></p>
-<p>2021 day <a href='/2021/19'>19</a></p>
-<p>2021 day <a href='/2021/20'>20</a></p>
-<p>2021 day <a href='/2021/21'>21</a></p>
-<p>2021 day <a href='/2021/22'>22</a></p>
-<p>2021 day <a href='/2021/23'>23</a></p>
-<p>2021 day <a href='/2021/24'>24</a></p>
-<p>2021 day <a href='/2021/25'>25</a></p>
+<p>Go to {year} day <a href='/{year}/{day}'>{day}</a></p>
 
-<p>2022 day <a href='/2022/01'>01</a></p>
-<p>2022 day <a href='/2022/02'>02</a></p>
-<p>2022 day <a href='/2022/03'>03</a></p>
-<p>2022 day <a href='/2022/04'>04</a></p>
-<p>2022 day <a href='/2022/05'>05</a></p>
-<p>2022 day <a href='/2022/06'>06</a></p>
-<p>2022 day <a href='/2022/07'>07</a></p>
-<p>2022 day <a href='/2022/08'>08</a></p>
-<p>2022 day <a href='/2022/09'>09</a></p>
-<p>2022 day <a href='/2022/10'>10</a></p>
-<p>2022 day <a href='/2022/11'>11</a></p>
-<p>2022 day <a href='/2022/12'>12</a></p>
-<p>2022 day <a href='/2022/13'>13</a></p>
-<p>2022 day <a href='/2022/14'>14</a></p>
-<p>2022 day <a href='/2022/15'>15</a></p>
-<p>2022 day <a href='/2022/16'>16</a></p>
-<p>2022 day <a href='/2022/17'>17</a></p>
-<p>2022 day <a href='/2022/18'>18</a></p>
-<p>2022 day <a href='/2022/19'>19</a></p>
-<p>2022 day <a href='/2022/20'>20</a></p>
-<p>2022 day <a href='/2022/21'>21</a></p>
-<p>2022 day <a href='/2022/22'>22</a></p>
-<p>2022 day <a href='/2022/23'>23</a></p>
-<p>2022 day <a href='/2022/24'>24</a></p>
-<p>2022 day <a href='/2022/25'>25</a></p>
